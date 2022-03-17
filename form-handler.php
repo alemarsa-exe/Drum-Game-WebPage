13e888b3095cb5cc36a57608013366af32986f50
Alex M <alex.mariacca@outlook.com>
2022-03-17 16:53:53 -0600

    Cambios

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@drumgame.mx';
+$email_from = 'info@drumgame.com';
 
 $email_subject = 'Nuevo mensaje de contacto';
 
